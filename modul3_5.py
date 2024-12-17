3e8c449dd200b1f41071531d3bac2177879242c6
NataliKicheeva <tusya21po@gmail.com>
2024-12-17 21:18:28 +0700

    Add files via upload

--- modul3_5.py
+++ modul3_5.py
@@ -3,10 +3,17 @@ def get_multiplied_digits(number):
 
     if len(str_number) > 1:
         first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number)
+        return int(str_number) if str_number != '0' else 1
 
-# Вызов функции и вывод результата
 result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
+
+
+
